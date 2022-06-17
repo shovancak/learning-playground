fa943bdec73cfeddcd42e0375de9dacc32b7c13b
shovancak <s.hovancak@gmail.com>
2022-06-17 22:26:36 +0200

    revert changes

--- src/api/resolvers/hello/hello.ts
+++ src/api/resolvers/hello/hello.ts
@@ -1,19 +1,7 @@
-import { PrismaClient } from '@prisma/client'
 import { QueryHelloArgs } from 'api/generated/resolvers-types'
-
-const Prisma = new PrismaClient()
 
 export default {
   Query: {
-    hello: async (_: unknown, { name }: QueryHelloArgs) => {
-      const user = await Prisma.user.findUnique({
-        where: {
-          id: 12,
-        },
-      })
-
-      console.log('user', user)
-      return `Hello ${name}!`
-    },
+    hello: (_: unknown, { name }: QueryHelloArgs) => `Hello ${name}!`,
   },
 }
