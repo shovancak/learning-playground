dee91bb2f915a88c9b8c780ce9906d992d86f506
shovancak <s.hovancak@gmail.com>
2022-06-17 21:50:16 +0200

    add test query for user

--- src/api/resolvers/hello/hello.ts
+++ src/api/resolvers/hello/hello.ts
@@ -1,7 +1,19 @@
+import { PrismaClient } from '@prisma/client'
 import { QueryHelloArgs } from 'api/generated/resolvers-types'
+
+const Prisma = new PrismaClient()
 
 export default {
   Query: {
-    hello: (_: unknown, { name }: QueryHelloArgs) => `Hello ${name}!`,
+    hello: async (_: unknown, { name }: QueryHelloArgs) => {
+      const user = await Prisma.user.findUnique({
+        where: {
+          id: 12,
+        },
+      })
+
+      console.log('user', user)
+      return `Hello ${name}!`
+    },
   },
 }
